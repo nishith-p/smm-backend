1a1dfc0ea975d92f82b8b0079ab790b8e3d7d25c
nishith-p <nishith.pinnawala@gmail.com>
2022-04-24 00:27:36 +0530

    Moved to mariadb

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,9 +1,16 @@
-import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Student } from 'src/students/entities/student.entity';
 
-const config: SqliteConnectionOptions = {
-  type: 'sqlite',
-  database: 'db',
-  entities: ['dist/src/students/entities/*.entity.js'],
+require('dotenv').config();
+
+const config: TypeOrmModuleOptions = {
+  type: 'mariadb',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.DB_USERNAME, 
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_DATABASE, 
+  entities: [Student],
   synchronize: true,
 };
 
